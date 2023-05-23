1cc87486a5b076d96862b80d09274a6664627614
johnechono <johninalegwu22@gmail.com>
2023-05-23 05:23:37 -0700

    builtin1 update

--- builtin1.c
+++ builtin1.c
@@ -1,118 +1,114 @@
 #include "shell.h"
 
 /**
- * find_key - finds an environment variable in the shell
- * @env: array of environment variables for the program's data
- * @key: environment variable to find
- *
- * Return: pointer to address of the environment variable
+ * find_key - will find the environ variable
+ * @env: environmental variable array
+ * @key: variable that should be found
+ * Return: pointer
  */
 char **find_key(char **env, char *key)
 {
-	unsigned int j, k, len;
+	unsigned int j, k, lens;
 
-	len = _strlen(key);
+	lens = _strlen(key);
 	for (j = 0; env[j] != NULL; j++)
 	{
-		for (k = 0; k < len; k++)
+		for (k = 0; k < lens; k++)
 			if (key[k] != env[j][k])
 				break;
-		if (k == len && env[j][k] == '=')
+		if (k == lens && env[j][k] == '=')
 			return (&env[j]);
 	}
 	return (NULL);
 }
 
 /**
- * add_key - create a new environment variable in the shell
- * @vars: pointer to struct of variables for the program's data
- *
- * Return: nothing
+ * add_key - will create a fresh environ variable
+ * @vars: variable struct pointer
+ * Return: void
  */
 void add_key(vars_t *vars)
 {
+	char **new_env;
 	unsigned int j;
-	char **newenv;
 
 	for (j = 0; vars->env[j] != NULL; j++)
 		;
-	newenv = malloc(sizeof(char *) * (j + 2));
-	if (newenv == NULL)
+	new_env = malloc(sizeof(char *) * (j + 2));
+	if (new_env == NULL)
 	{
 		print_error(vars, NULL);
 		vars->status = 127;
 		new_exit(vars);
 	}
 	for (j = 0; vars->env[j] != NULL; j++)
-		newenv[j] = vars->env[j];
-	newenv[j] = add_value(vars->av[1], vars->av[2]);
-	if (newenv[j] == NULL)
+		new_env[j] = vars->env[j];
+	new_env[j] = add_value(vars->av[1], vars->av[2]);
+	if (new_env[j] == NULL)
 	{
 		print_error(vars, NULL);
 		free(vars->buffer);
-		free(vars->commands);
 		free(vars->av);
 		free_env(vars->env);
-		free(newenv);
+		free(vars->commands);
+		free(new_env);
 		exit(127);
 	}
-	newenv[j + 1] = NULL;
+	new_env[j + 1] = NULL;
 	free(vars->env);
-	vars->env = newenv;
+	vars->env = new_env;
 }
 
 /**
- * add_value - creates a new environment variable string in the shell
- * @key: variable name for the program's data
- * @value: variable value
- *
- * Return: 0 if success, or pointer to the string
+ * add_value - will create fresh variable
+ * @key: name of variable
+ * @value: value of the variable
+ * Return: pointer or 0 on success
  */
 char *add_value(char *key, char *value)
 {
-	unsigned int len1, len2, j, k;
+	unsigned int lens1, lens2, j, k;
 	char *new;
 
-	len1 = _strlen(key);
-	len2 = _strlen(value);
-	new = malloc(sizeof(char) * (len1 + len2 + 2));
+	lens2 = _strlen(value);
+	lens1 = _strlen(key);
+	new = malloc(sizeof(char) * (lens1 + lens2 + 2));
 	if (new == NULL)
 		return (NULL);
+	for (k = 0; value[k] != '\0'; k++)
+		new[j + 1 + k] = value[k];
+        new[j + 1 + k] = '\0';
 	for (j = 0; key[j] != '\0'; j++)
 		new[j] = key[j];
 	new[j] = '=';
-	for (k = 0; value[k] != '\0'; k++)
-		new[j + 1 + k] = value[k];
-	new[j + 1 + k] = '\0';
 	return (new);
 }
 
 /**
- * _atoi - converts a string into an integer
- * @str: the string to be converted
- *
- * Return: the integer value converted, or -1 if an error occurs
+ * _atoi - will convert str to int
+ * @str: converted string
+ * Return: -1 for error or int value
  */
 int _atoi(char *str)
 {
-	unsigned int j, digits;
-	int num = 0, num_test;
+	unsigned int j, dig;
+	int nums = 0, test_num;
 
-	num_test = INT_MAX;
-	for (digits = 0; num_test != 0; digits++)
-		num_test /= 10;
-	for (j = 0; str[j] != '\0' && j < digits; j++)
+	test_num = INT_MAX;
+	for (dig = 0; test_num != 0; dig++)
+		test_num /= 10;
+	for (j = 0; str[j] != '\0' && j < dig; j++)
 	{
-		num *= 10;
+		nums *= 10;
+		if ((j == dig - 1) && (str[j] - '0' > INT_MAX % 10))
+			return (-1);
 		if (str[j] < '0' || str[j] > '9')
 			return (-1);
-		if ((j == digits - 1) && (str[j] - '0' > INT_MAX % 10))
-			return (-1);
-		num += str[j] - '0';
-		if ((j == digits - 2) && (str[j + 1] != '\0') && (num > INT_MAX / 10))
+		nums += str[j] - '0';
+		if ((j == dig - 2) && (str[j + 1] != '\0') && (nums > INT_MAX / 10))
 			return (-1);
 	}
-	if (j > digits)
+	if (j > dig)
 		return (-1);
-	return (num);
+	return (nums);
 }
