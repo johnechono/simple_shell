978ae3b3618f7cbb53ea6b2337d44de35175c672
Boolayinka <boolayinka@gmail.com>
2023-05-24 07:22:05 -0700

    simple_shell

---
+++ environ_get.c
@@ -0,0 +1,90 @@
+#include "shell.h"
+
+/**
+ * get_environ - returns the string array copy of environ
+ * @info: Used to maintain constant function prototype.
+ * Return: Always 0
+ */
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _setenv - Initialize new environment var
+ * @info: Used to maintain constant function prototype.
+ * @var: string env
+ * @value: string env var value
+ *  Return: Always 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *q;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		q = starts_with(node->str, var);
+		if (q && *q == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
+
+/**
+ * _unsetenv - Remove an environment var
+ * @info: Used to maintain constant function prototype.
+ * Return: 1
+ * O/W: 0
+ * @var: string env var
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t j = 0;
+	char *q;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		q = starts_with(node->str, var);
+		if (q && *q == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), j);
+			j = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		j++;
+	}
+	return (info->env_changed);
+}
