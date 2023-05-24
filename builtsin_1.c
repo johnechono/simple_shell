bef0e5e3137b9b2bbbf5f848a9765d2b0c00c38c
Boolayinka <boolayinka@gmail.com>
2023-05-24 06:03:33 -0700

    simple_shell

---
+++ builtsin_1.c
@@ -0,0 +1,110 @@
+#include "shell.h"
+
+/**
+ * unset_alias - sets alias
+ * @str: alias string
+ * @info: parameter
+ * Return: Always 0 on success, 1 on error otherwise
+ */
+int unset_alias(info_t *info, char *str)
+{
+	int ret;
+	char *q, d;
+
+	q = _strchr(str, '=');
+	if (!q)
+		return (1);
+	d = *q;
+	*q = 0;
+	ret = delete_node_at_index(&(info->alias),
+			get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*q = d;
+	return (ret);
+}
+
+/**
+ * set_alias - sets alias
+ * @str: alias string
+ * @info: parameter
+ * Return: Always 0 on success, otherwise 1 on error
+ */
+int set_alias(info_t *info, char *str)
+{
+	char *q;
+
+	q = _strchr(str, '=');
+	if (!q)
+		return (1);
+	if (!*++q)
+		return (unset_alias(info, str));
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
+/**
+ * _myhistory - displays the history list
+ * @info: Used to maintain constant function
+ *  Return: Always 0
+ */
+int _myhistory(info_t *info)
+{
+	print_list(info->history);
+	return (0);
+}
+
+/**
+ * print_alias - prints an alias
+ * @node: the node
+ * Return: Always 0 on success, otherwise 1 on error
+ */
+int print_alias(list_t *node)
+{
+	char *q = NULL;
+	char *b = NULL;
+
+	if (node)
+	{
+		q = _strchr(node->str, '=');
+		for (b = node->str; b <= q; b++)
+			_putchar(*b);
+		_putchar('\'');
+		_puts(p + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * _myalias - mimics the alias
+ * @info: Used to maintain constant function prototype.
+ * Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+	int j = 0;
+	char *q = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (j = 1; info->argv[j]; j++)
+	{
+		q = _strchr(info->argv[j], '=');
+	if (q)
+		set_alias(info, info->argv[i]);
+	else
+		print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
---
+++ environ.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * _myenv - prints current environment
+ * @info: Used to maintain constant function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env list
+ * @info: Used to maintain constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t j;
+
+	for (j = 0; environ[j]; j++)
+		add_node_end(&node, environ[j], 0);
+	info->env = node;
+	return (0);
+}
+
+/**
+ * _getenv - gets the value of an environ var
+ * @info: Used to maintain
+ * @name: environment variable name
+ * Return: value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *q;
+
+	while (node)
+	{
+		q = starts_with(node->str, name);
+		if (q && *q)
+			return (q);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - Initialize a new environment variable
+ * @info: structure containing potential argument
+ * Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments.
+ * Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int j;
+
+	if (info->argc == 1)
+	{
+		_eputs("Few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
