79af2b494ffbf71be960ffb0362c32fbe7fb1ab8
johnechono <johninalegwu22@gmail.com>
2023-05-24 07:16:22 -0700

    update

---
+++ list_1.c
@@ -0,0 +1,117 @@
+#include "shell.h"
+
+/**
+ * list_len - length of list
+ * @h: pointer
+ * Return: size
+ */
+size_t list_len(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		h = h->next;
+		j++;
+	}
+	return (j);
+}
+
+/**
+ * list_to_strings - list to strings
+ * @head: pointer
+ * Return: strings
+ */
+char **list_to_strings(list_t *head)
+{
+	char **strs;
+	char *str;
+	list_t *node = head;
+	size_t j = list_len(head), k;
+
+	if (!head || !j)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (j + 1));
+	if (!strs)
+		return (NULL);
+	for (j = 0; node; node = node->next, j++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (k = 0; k < j; k++)
+				free(strs[k]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[j] = str;
+	}
+	strs[j] = NULL;
+	return (strs);
+}
+
+/**
+ * node_starts_with - node starting with prefix
+ * @prefix: string
+ * @node: pointer
+ * @c: the next character after prefix to match
+ * Return: null
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *q = NULL;
+
+	while (node)
+	{
+		q = starts_with(node->str, prefix);
+		if (q && ((c == -1) || (*q == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * get_node_index - gets index
+ * @head: head pointer
+ * @node: node pointer
+ * Return: index
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t j = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (j);
+		head = head->next;
+		j++;
+	}
+	return (-1);
+}
+
+/**
+ * print_list - prints all elements of a list_t linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		j++;
+	}
+	return (j);
+}
